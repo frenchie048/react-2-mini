2fe4fcb5026709a8cffd9e35d45390318f287a23
frenchie048 <37281870+frenchie048@users.noreply.github.com>
2019-01-21 13:19:57 -0700

    completed full mini

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -6,6 +6,7 @@ export default class TextContainer extends Component {
     this.state = {
       text: ''
     }
+    this.updateText = this.updateText.bind(this)
   }
 
   updateText(e) {
@@ -17,15 +18,15 @@ export default class TextContainer extends Component {
   render() {
     return (
       <div className="textContainer">
-        <textarea 
-          // style={  }
+        <textarea
+          style={{ color: this.props.fontColor, fontSize: this.props.fontSize, fontFamily: this.props.fontFamily }}
           onChange={this.updateText}
-          value={this.state.text} 
+          value={this.state.text}
           placeholder='Start typing your thoughts here!'
           cols="90"
           rows="30">
         </textarea>
-      </div>
+      </div >
     )
   }
 }
